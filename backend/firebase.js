ed079eb317b06aec0d33ef155394c449462c9006
MohsinAliJafery <70712589+MohsinAliJafery@users.noreply.github.com>
2024-11-27 21:13:35 +0500

    Add files via upload

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,12 +1,15 @@
 // firebase.js
+require('dotenv').config(); // Load environment variables from .env file
 const admin = require('firebase-admin');
-const path  = require('path');
+const path = require('path');
 
-const serviceAccount = path.resolve('./movie-app-60d18-firebase-adminsdk-uhcm7-7ecd0962f8.json');
+// Get the service account key path from environment variable
+const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH;
 
+// Initialize Firebase with credentials from the environment
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'gs://mechanic-hub-60d18.appspot.com' // Replace with your Firebase Storage bucket
+  credential: admin.credential.cert(require(serviceAccountPath)), // Read the key file from the path
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET, // Use the storage bucket from the environment variable
 });
 
 const bucket = admin.storage().bucket();
